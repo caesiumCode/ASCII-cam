027ad566ebc413ba81da39cae491e17bdda606f0
caesiumCode <caesiumcode@aol.com>
2022-03-12 12:34:48 +1100

    Revert "Initial Commit"
    This reverts commit 9771d37669e11b0dc1d3bac476046cb2a594d9d7.

---
+++ ASCII-cam/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ASCII-cam
+//
+//  Created by Stephen Jaud on 12/3/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
